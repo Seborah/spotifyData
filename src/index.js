6a3c61805d87e73b890761c28ed1b05a6622ac24
Seborah <sebhase@gmail.com>
2022-09-19 15:34:57 -0400

    add mongo

--- src/index.js
+++ src/index.js
@@ -1,28 +1,46 @@
 var SpotifyWebApi = require("spotify-web-api-node")
+
 var auth = require("./auth.json")
 
-var spotifyApi = new SpotifyWebApi(auth)
+var spotifyApi = new SpotifyWebApi(auth.SpotifyAPI)
 
 const SpotifyStrategy = require("passport-spotify").Strategy
-var User = require("./models/User.js")
 const passport = require("passport")
+
+var mongoose = require("mongoose")
+mongoose.connect(
+	auth.Mongoose.URI,
+	{
+		auth: {
+			username: auth.Mongoose.username,
+			password: auth.Mongoose.password,
+		},
+	},
+	(err) => {
+		if (err) console.log(err)
+		else console.log("Connected to MongoDB")
+	}
+)
+
+var User = require("./models/User.js")
 
 passport.use(
 	new SpotifyStrategy(
 		{
-			clientID: auth.clientId,
-			clientSecret: auth.clientSecret,
+			clientID: auth.SpotifyAPI.clientId,
+			clientSecret: auth.SpotifyAPI.clientSecret,
 			callbackURL: "http://localhost:8888/auth/spotify/callback",
-        },
-        
-		function (accessToken, refreshToken, expires_in, profile, done) {
-            new User({
-                spotifyId: profile.id,
-                accessToken: accessToken,
-                refreshToken: refreshToken,
-                expires_in: expires_in,
-                profile: profile
-            }).save()
+		},
+
+		(accessToken, refreshToken, expires_in, profile, done) => {
+			var temp = new User({
+				spotifyId: profile.id,
+				accessToken: accessToken,
+				refreshToken: refreshToken,
+				expires_in: expires_in,
+				profile: profile,
+            })
+            temp.save()
 		}
 	)
 )
@@ -36,7 +54,6 @@ app.use(require("body-parser").urlencoded({ extended: true }))
 app.use(require("express-session")({ secret: "Sakura", resave: true, saveUninitialized: true }))
 app.use(passport.initialize())
 app.use(passport.session())
-
 
 app.get("/auth/spotify", passport.authenticate("spotify"))
 
